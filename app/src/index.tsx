f2538f17117238c35c6bafea75796a126d7b7116
kazuki-saito803 <kaz.saito803@gmail.com>
2025-08-13 23:01:55 +0900

    create custombutton

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-import SendButton from './components/sendButton'; // コンポーネント名を大文字で始める
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +10,5 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
-    <SendButton type="submit" onClick={() => alert('送信しました！')}>送信</SendButton>
   </React.StrictMode>
 );
